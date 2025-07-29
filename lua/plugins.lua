50bcf5bfb4ffdab7d598960114adb89593e5811e
sayemon10 <abdulaziz.aziz99@gmail.com>
2025-07-30 00:37:00 +0600

    Look: Github Theme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -123,10 +123,27 @@ return {
         end,
     },
     { 'mattn/emmet-vim' },
+    --    {
+    --        'morhetz/gruvbox',
+    --        config = function()
+    --            vim.cmd('colorscheme gruvbox')
+    --        end,
+    --    },
+    -- Install without configuration
+    { 'projekt0n/github-nvim-theme', name = 'github-theme' },
+
+    -- Or with configuration
     {
-        'morhetz/gruvbox',
+        'projekt0n/github-nvim-theme',
+        name = 'github-theme',
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
-            vim.cmd('colorscheme gruvbox')
+            require('github-theme').setup({
+                -- ...
+            })
+
+            vim.cmd('colorscheme github_dark')
         end,
     },
     {

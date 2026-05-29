e386d83a58f9539cd28423715e64fbb19b58a141
sayemon10 <abdulaziz.aziz99@gmail.com>
2026-05-29 10:51:16 +0600

    ~

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,9 +5,9 @@ return {
         'github/copilot.vim',
         lazy = false,
         config = function()
+            vim.g.copilot_assume_mapped = true
             vim.g.copilot_no_tab_map = true
             vim.api.nvim_set_keymap('i', '<C-Tab>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
-            vim.g.copilot_assume_mapped = true
         end,
     },
     {

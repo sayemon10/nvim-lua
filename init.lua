84a146f4121a1499237d2069dca68d766e642dc8
sayemon10 <abdulaziz.aziz99@gmail.com>
2026-09-09 15:21:40 +0600

    ~

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@
 -- Author: sayemon10
 -- URL: https://www.sayemon10.com
 
+require("vim._core.ui2").enable({})
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
